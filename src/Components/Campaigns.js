44c909a974edb2007eac00d49081654313a72d49
Ifeoluwa Sanni <pheobeayo@gmail.com>
2024-07-19 17:55:43 +0100

    campaign-page

--- src/Components/Campaigns.js
+++ src/Components/Campaigns.js
@@ -1,69 +1,127 @@
 import React from "react";
+import CampaignImg from "../Images/campaign2.jpg";
+import CampaignImg2 from "../Images/campaign4.jpg";
+import CampaignImg3 from "../Images/campaign5.jpg";
 import { motion } from "framer-motion";
 
 const Campaigns = ({ show }) => {
-  const variants = {
-    hidden: { opacity: 0 },
-    show: {
-      opacity: 1,
-      transition: {
-        staggerChildren: 0.2,
-      },
-    },
-  };
-
-  const item = {
-    hidden: { opacity: 0, x: -20 },
-    show: {
-      opacity: 1,
-      x: 0,
-      transition: {
-        duration: 0.3,
-      },
-    },
-  };
-
-
-
   return (
     <div
-      className={`${show && "blur-sm"}  pt-2 px-2 sm:px-5 md:px-12 lg:pl-24 ff`}
+      className={`${show && "blur-sm"}  pt-2 px-2 sm:px-5 md:px-20 lg:pl-24 ff`}
     >
-      <div className="mb-10">
-        <h1 className="text-xl sm:text-2xl py-3 text-[#facc15]">Campaigns</h1>
-        <p className=" font-light text-[#facc15]">
-          Our Campaigns include:
-        </p>
-        <div className="flex justify-center items-center">
-          <motion.div
-            variants={variants}
-            initial="hidden"
-            animate="show"
-            transition={{ duration: 0.01, delay: 0.05, staggerChildren: 0.2 }}
-            className="text-center flex flex-wrap justify-center items-center text-name sm:w-2/3 md:w-1/2"
-          >
-            <motion.p
-              variants={item}
-              className="bg-[#233554] px-5 py-3 m-2 hover:text-white  hover:px-6"
+      <h1 className="text-xl sm:text-2xl py-3 text-[#facc15]">
+        Our Campaigns 
+        </h1>
+      <div>
+        <section className="pb-5">
+          <h2 className="text-name sm:text-xl py-3">
+            <span className="text-white">01.</span> Awareness Program
+          </h2>
+          <div className="flex justify-center items-center flex-col sm:flex-row ">
+            <motion.article
+              initial={{ opacity: 0.5, x: 100 }}
+              whileInView={{
+                opacity: 1,
+                x: 0,
+              }}
+              transition={{ duration: 1 }}
+              className="sm:w-1/2 bg-[#233554] p-4 text-[#facc15]"
             >
               We are committed to providing exceptional mental health support, raising awareness,
-              <br /> and giving back to society by creating a
-              community where everyone feels understood,
-              <br />valued, and empowered to lead fulfilling lives.
-            </motion.p>
-            <motion.p
-              variants={item}
-              className="bg-[#233554] px-5 py-3 m-2 hover:text-white  hover:px-6"
+              and giving back to society by creating a community where everyone feels understood,valued,
+              and empowered to lead fulfilling lives.
+
+
+            </motion.article>
+            <motion.img
+              initial={{ opacity: 0.5, x: -100 }}
+              whileInView={{
+                opacity: 1,
+                x: 0,
+              }}
+              transition={{ duration: 1 }}
+              src={CampaignImg}
+              alt="campaign2"
+              className="sm:w-1/2 rounded-b-lg sm:rounded-lg"
+            />
+          </div>
+        </section>
+      </div>
+
+      <div>
+        <section className="pb-5">
+          <h2 className="text-name sm:text-xl py-3">
+            <span className="text-white">02.</span> Giving help to the Needy
+          </h2>
+          <div className="flex justify-center items-center flex-col sm:flex-row ">
+            <motion.img
+              initial={{ opacity: 0.5, x: -100 }}
+              whileInView={{
+                opacity: 1,
+                x: 0,
+              }}
+              transition={{ duration: 1 }}
+              src={CampaignImg2}
+              alt="campaign4"
+              className="sm:w-1/2 rounded-b-lg sm:rounded-lg"
+            />
+            <motion.article
+              initial={{ opacity: 0.5, x: 100 }}
+              whileInView={{
+                opacity: 1,
+                x: 0,
+              }}
+              transition={{ duration: 1 }}
+              className="sm:w-1/2 bg-[#233554] p-4 text-[#facc15]"
             >
               Giving back to the society
               Through raising awareness about mental health support
-            </motion.p>
-           
-            
-          </motion.div>
-        </div>
+
+
+            </motion.article>
+
+          </div>
+        </section>
       </div>
-      
+
+      <div>
+        <section className="pb-5">
+          <h2 className="text-name sm:text-xl py-3">
+            <span className="text-white">03.</span> Enlightment Campaign
+          </h2>
+          <div className="flex justify-center items-center flex-col sm:flex-row ">
+            <motion.article
+              initial={{ opacity: 0.5, x: 100 }}
+              whileInView={{
+                opacity: 1,
+                x: 0,
+              }}
+              transition={{ duration: 1 }}
+              className="sm:w-1/2 bg-[#233554] p-4 text-[#facc15]"
+            >
+              Giving Succour to the less priviledged is our Joy
+            </motion.article>
+            <motion.img
+              initial={{ opacity: 0.5, x: -100 }}
+              whileInView={{
+                opacity: 1,
+                x: 0,
+              }}
+              transition={{ duration: 1 }}
+              src={CampaignImg3}
+              alt="campaign5"
+              className="sm:w-1/2 rounded-b-lg sm:rounded-lg"
+            />
+          </div>
+        </section>
+      </div>
+
+
+
+
+
+
+
     </div>
   );
 };
